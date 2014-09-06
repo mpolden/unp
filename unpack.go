9e64c1e23f4395a7f7e647e250c1ddce144a3e81
Martin Polden <martin.polden@gmail.com>
2014-09-06 17:25:35 +0200

    Detect and verify SFV

--- unpack.go
+++ unpack.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/martinp/gosfv"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -39,6 +41,53 @@ func parsePaths() ([]Path, error) {
 	return paths, nil
 }
 
+func findSFV(dir string) (string, error) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return "", err
+	}
+	for _, f := range files {
+		if filepath.Ext(f.Name()) == ".sfv" {
+			return filepath.Join(dir, f.Name()), nil
+		}
+	}
+	return "", nil
+}
+
+func unpack(e *Event, p *Path) {
+	log.Printf("event %s", e)
+	log.Printf("path %s", p)
+	log.Printf("dir %s", filepath.Dir(e.Name))
+	dir := filepath.Dir(e.Name)
+	sfvPath, err := findSFV(dir)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if sfvPath == "" {
+		log.Print("No SFV found")
+	} else {
+		log.Printf("sfv: %s", sfvPath)
+	}
+	sfvFile, err := sfv.Read(sfvPath)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if sfvFile.IsExist() {
+		log.Printf("all files in sfv exist!")
+		log.Printf("verifying sfv")
+		ok, err := sfvFile.Verify()
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		if ok {
+			log.Printf("unpack!")
+		}
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		usage()
@@ -59,9 +108,7 @@ func main() {
 	if err := w.AddWatch(paths); err != nil {
 		log.Fatal(err)
 	}
-	w.OnFile = func(e *Event, p *Path) {
-		log.Printf("called with %s in %s", e, p)
-	}
+	w.OnFile = unpack
 	w.Serve()
 
 }
