d9cf634133a4a55b98e89dd1e395f81f9c8532e7
Martin Polden <martin.polden@gmail.com>
2014-09-06 17:28:09 +0200

    Split main and unpack

--- unpack.go
+++ unpack.go
@@ -1,45 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/martinp/gosfv"
 	"io/ioutil"
 	"log"
-	"os"
-	"os/exec"
 	"path/filepath"
-	"strconv"
-	"strings"
 )
-
-func usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s path:level [path:level ...]\n",
-		os.Args[0])
-	os.Exit(1)
-}
-
-func parsePaths() ([]Path, error) {
-	paths := make([]Path, 0, len(os.Args[1:]))
-	for _, arg := range os.Args[1:] {
-		s := strings.Split(arg, ":")
-		if len(s) != 2 {
-			return nil, fmt.Errorf("expected path:depth, got %s",
-				arg)
-		}
-		path := filepath.Clean(s[0])
-		depth, err := strconv.Atoi(s[1])
-		if depth < 1 || err != nil {
-			return nil, fmt.Errorf(
-				"depth must be an positive integer")
-		}
-		p := Path{
-			Name:     path,
-			MaxDepth: depth,
-		}
-		paths = append(paths, p)
-	}
-	return paths, nil
-}
 
 func findSFV(dir string) (string, error) {
 	files, err := ioutil.ReadDir(dir)
@@ -86,29 +52,4 @@ func unpack(e *Event, p *Path) {
 			log.Printf("unpack!")
 		}
 	}
-}
-
-func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
-	if _, err := exec.LookPath("unrar"); err != nil {
-		log.Fatal(err)
-	}
-
-	paths, err := parsePaths()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	w, err := New()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := w.AddWatch(paths); err != nil {
-		log.Fatal(err)
-	}
-	w.OnFile = unpack
-	w.Serve()
-
 }
