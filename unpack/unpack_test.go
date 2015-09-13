f7f88f085b4fbae703b52a8c48bd673c7c9fe2e5
Martin Polden <martin.polden@gmail.com>
2015-09-13 15:22:22 +0200

    Fix tests

--- unpack/unpack_test.go
+++ unpack/unpack_test.go
@@ -1,49 +1,24 @@
 package unpack
 
 import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"testing"
+
+	sfv "github.com/martinp/gosfv"
 )
 
-func TestFindSFV(t *testing.T) {
-	dir, err := ioutil.TempDir("", "gounpack")
+func TestFindArchive(t *testing.T) {
+	sfv := &sfv.SFV{Checksums: []sfv.Checksum{
+		sfv.Checksum{Path: "foo.r00"},
+		sfv.Checksum{Path: "foo.rar"},
+	}}
+	archive, err := findArchive(sfv, ".rar")
 	if err != nil {
 		t.Fatal(err)
 	}
-	file := filepath.Join(dir, "test.sfv")
-	if err := ioutil.WriteFile(file, []byte{}, 0600); err != nil {
-		t.Fatal(err)
+	if expected := "foo.rar"; archive != expected {
+		t.Errorf("Expected %q, got %q", expected, archive)
 	}
-	sfv, err := findSFV(dir)
-	if err != nil {
-		t.Fatal(err)
+	if _, err := findArchive(sfv, ".bar"); err == nil {
+		t.Error("Expected error")
 	}
-	if sfv != file {
-		t.Fatalf("Expected %s, got %s", file, sfv)
-	}
-	if err := os.Remove(file); err != nil {
-		t.Fatal(err)
-	}
-	if _, err := findSFV(dir); err == nil {
-		t.Fatal("Expected error")
-	}
-	os.Remove(dir) // ignore error
-}
-
-func TestReadSFV(t *testing.T) {
-	dir, err := ioutil.TempDir("", "gounpack")
-	if err != nil {
-		t.Fatal(err)
-	}
-	file := filepath.Join(dir, "test.sfv")
-	const sfv = "foo 7E3265A8\nbar 04A2B3E9"
-	if err := ioutil.WriteFile(file, []byte(sfv), 0600); err != nil {
-		t.Fatal(err)
-	}
-	if _, err := readSFV(dir); err != nil {
-		t.Fatal(err)
-	}
-	os.Remove(dir) // ignore error
 }
