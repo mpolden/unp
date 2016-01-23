974f7c51f04b989a7c44bbe5631a4fe4961ecd3b
Martin Polden <martin.polden@gmail.com>
2016-01-23 12:19:57 +0100

    Test config defaults

--- dispatcher/config_test.go
+++ dispatcher/config_test.go
@@ -1,8 +1,46 @@
 package dispatcher
 
 import (
+	"strings"
 	"testing"
 )
+
+func TestReadConfig(t *testing.T) {
+	jsonConfig := `
+{
+  "Default": {
+    "MaxDepth": 3
+  },
+  "Paths": [
+    {
+      "Name": "/tmp/foo"
+    },
+    {
+      "Name": "/tmp/bar",
+      "MaxDepth": 4
+    }
+  ]
+}
+`
+	cfg, err := readConfig(strings.NewReader(jsonConfig))
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Test that defaults are applied and can be overridden
+	var tests = []struct {
+		i   int
+		out int
+	}{
+		{0, 3},
+		{1, 4},
+	}
+	for _, tt := range tests {
+		path := cfg.Paths[tt.i]
+		if got := path.MaxDepth; got != tt.out {
+			t.Errorf("Expected MaxDepth=%d, got Parser=%d for Name=%q", tt.out, got, path.Name)
+		}
+	}
+}
 
 func TestFindPath(t *testing.T) {
 	c := Config{Paths: []Path{Path{Name: "/foo/bar"}}}
