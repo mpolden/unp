41cc5fbc905511fc723b0f1b42c2cd4d72e7a81c
Martin Polden <martin.polden@gmail.com>
2017-03-11 15:47:55 +0100

    Reduce public members in unpacker package

--- unpacker/unpacker.go
+++ unpacker/unpacker.go
@@ -18,7 +18,7 @@ type unpacker struct {
 	Archive string
 }
 
-func New(e dispatcher.Event, p dispatcher.Path) (*unpacker, error) {
+func newUnpacker(e dispatcher.Event, p dispatcher.Path) (*unpacker, error) {
 	sfv, err := sfv.Find(e.Dir())
 	if err != nil {
 		return nil, err
@@ -44,7 +44,7 @@ func findArchive(s *sfv.SFV, ext string) (string, error) {
 	return "", fmt.Errorf("no archive file found in %s", s.Path)
 }
 
-func (u *unpacker) Run() error {
+func (u *unpacker) unpack() error {
 	values := dispatcher.CmdValues{
 		Name: u.Archive,
 		Base: u.Event.Base(),
@@ -60,7 +60,7 @@ func (u *unpacker) Run() error {
 	return nil
 }
 
-func (u *unpacker) PostRun() error {
+func (u *unpacker) postProcess() error {
 	if u.Path.PostCommand == "" {
 		return nil
 	}
@@ -81,7 +81,7 @@ func (u *unpacker) PostRun() error {
 	return nil
 }
 
-func (u *unpacker) Remove() error {
+func (u *unpacker) remove() error {
 	if !u.Path.Remove {
 		return nil
 	}
@@ -93,7 +93,7 @@ func (u *unpacker) Remove() error {
 	return os.Remove(u.SFV.Path)
 }
 
-func (u *unpacker) FileCount() (int, int) {
+func (u *unpacker) fileCount() (int, int) {
 	exists := 0
 	for _, c := range u.SFV.Checksums {
 		if c.IsExist() {
@@ -103,7 +103,7 @@ func (u *unpacker) FileCount() (int, int) {
 	return exists, len(u.SFV.Checksums)
 }
 
-func (u *unpacker) Verify() error {
+func (u *unpacker) verify() error {
 	for _, c := range u.SFV.Checksums {
 		ok, err := c.Verify()
 		if err != nil {
@@ -117,36 +117,33 @@ func (u *unpacker) Verify() error {
 }
 
 func OnFile(e dispatcher.Event, p dispatcher.Path, log *log.Logger) {
-	u, err := New(e, p)
+	u, err := newUnpacker(e, p)
 	if err != nil {
 		log.Printf("Failed to create unpacker: %s", err)
 		return
 	}
 
-	if exists, total := u.FileCount(); exists != total {
-		log.Printf("%s: %d/%d files", u.Event.Dir(), exists, total)
+	exists, total := u.fileCount()
+	log.Printf("%s: %d/%d files", u.Event.Dir(), exists, total)
+	if exists != total {
 		return
 	}
 
-	// Verify
-	if err := u.Verify(); err != nil {
+	if err := u.verify(); err != nil {
 		log.Printf("Verification failed: %s", err)
 		return
 	}
 
-	// Unpack
-	if err := u.Run(); err != nil {
+	if err := u.unpack(); err != nil {
 		log.Printf("Unpacking failed: %s", err)
 		return
 	}
 
-	// Clean up
-	if err := u.Remove(); err != nil {
+	if err := u.remove(); err != nil {
 		log.Printf("Failed to delete files: %s", err)
 	}
 
-	// Run post-command
-	if err := u.PostRun(); err != nil {
-		log.Printf("Failed to run post-command: %s", err)
+	if err := u.postProcess(); err != nil {
+		log.Printf("Failed to run post-process command: %s", err)
 	}
 }
