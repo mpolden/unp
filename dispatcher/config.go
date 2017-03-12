ee1c9828a2b4902ceffb7f40f5d7966b4f9f0e8d
Martin Polden <martin.polden@gmail.com>
2017-03-12 12:42:56 +0100

    Move Path to config

--- dispatcher/config.go
+++ dispatcher/config.go
@@ -9,6 +9,8 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 type Config struct {
@@ -16,6 +18,35 @@ type Config struct {
 	BufferSize int
 	Paths      []Path
 	filename   string
+}
+
+type Path struct {
+	Name          string
+	MaxDepth      int
+	MinDepth      int
+	SkipHidden    bool
+	Patterns      []string
+	Remove        bool
+	ArchiveExt    string
+	UnpackCommand string
+	PostCommand   string
+}
+
+func (p *Path) match(name string) (bool, error) {
+	for _, pattern := range p.Patterns {
+		matched, err := filepath.Match(pattern, name)
+		if err != nil {
+			return false, errors.Wrap(err, pattern)
+		}
+		if matched {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func (p *Path) validDepth(depth int) bool {
+	return depth >= p.MinDepth && depth <= p.MaxDepth
 }
 
 func readConfig(r io.Reader) (Config, error) {
--- dispatcher/config_test.go
+++ dispatcher/config_test.go
@@ -64,3 +64,44 @@ func TestFindPath(t *testing.T) {
 		}
 	}
 }
+
+func TestPathMatch(t *testing.T) {
+	var tests = []struct {
+		p   Path
+		in  string
+		out bool
+		err string
+	}{
+		{Path{Patterns: []string{"*.txt"}}, "foo.txt", true, ""},
+		{Path{Patterns: []string{"*.txt"}}, "foo", false, ""},
+		{Path{Patterns: []string{"[bad pattern"}}, "foo", false, "[bad pattern: syntax error in pattern"},
+	}
+
+	for _, tt := range tests {
+		rv, err := tt.p.match(tt.in)
+		if err != nil && err.Error() != tt.err {
+			t.Fatalf("Expected error %q, got %q", tt.err, err.Error())
+		}
+		if rv != tt.out {
+			t.Errorf("Expected %t, got %t", tt.out, rv)
+		}
+	}
+}
+
+func TestValidDepth(t *testing.T) {
+	var tests = []struct {
+		in  int
+		out bool
+	}{
+		{3, false},
+		{4, true},
+		{5, true},
+		{6, false},
+	}
+	p := Path{MinDepth: 4, MaxDepth: 5}
+	for _, tt := range tests {
+		if rv := p.validDepth(tt.in); rv != tt.out {
+			t.Errorf("Expected %t, got %t", tt.out, rv)
+		}
+	}
+}
