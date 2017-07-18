193d254418db6ee55b159e0ecdcbe3a684f1700a
Martin Polden <mpolden@mpolden.no>
2017-07-18 17:07:51 +0200

    Test config reloading

--- watcher/watcher_test.go
+++ watcher/watcher_test.go
@@ -1,6 +1,7 @@
 package watcher
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -27,8 +28,19 @@ func testWatcher(dir string, onFile OnFile) *watcher {
 		BufferSize: 10,
 		Paths:      []Path{{Name: dir, MaxDepth: 100, Patterns: []string{"*"}}},
 	}
-	log := log.New(ioutil.Discard, "", log.LstdFlags)
+	log := log.New(ioutil.Discard, "", 0)
 	return New(cfg, onFile, log)
+}
+
+func awaitFile(files *[]string, file string) (bool, error) {
+	ts := time.Now()
+	for len(*files) == 0 {
+		time.Sleep(10 * time.Millisecond)
+		if time.Since(ts) > 2*time.Second {
+			return false, fmt.Errorf("timed out waiting for file notification")
+		}
+	}
+	return (*files)[0] == file, nil
 }
 
 func TestWatching(t *testing.T) {
@@ -48,15 +60,11 @@ func TestWatching(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Sleep until file is caught
-	ts := time.Now()
-	for len(files) == 0 {
-		time.Sleep(10 * time.Millisecond)
-		if time.Since(ts) > 2*time.Second {
-			t.Fatal("timed out waiting for file notification")
-		}
+	ok, err := awaitFile(&files, f)
+	if err != nil {
+		t.Fatal(err)
 	}
-	if files[0] != f {
+	if !ok {
 		t.Errorf("want %s, got %s", f, files[0])
 	}
 }
@@ -88,14 +96,57 @@ func TestRescanning(t *testing.T) {
 	w.signal <- syscall.SIGUSR1
 
 	// Sleep until file is caught
-	ts := time.Now()
-	for len(files) == 0 {
-		time.Sleep(10 * time.Millisecond)
-		if time.Since(ts) > 2*time.Second {
-			t.Fatal("timed out waiting for file notification")
-		}
+	ok, err := awaitFile(&files, f)
+	if err != nil {
+		t.Fatal(err)
 	}
-	if files[0] != f {
+	if !ok {
+		t.Errorf("want %s, got %s", f, files[0])
+	}
+}
+
+func TestReloading(t *testing.T) {
+	var files []string
+	w := testWatcher("", func(name, postCommand string, remove bool) error {
+		files = append(files, name)
+		return nil
+	})
+	defer w.Stop()
+
+	cfgFile, err := ioutil.TempFile("", "unpacker")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(cfgFile.Name())
+	w.config = Config{filename: cfgFile.Name()}
+
+	// Start serving with empty config
+	w.goServe()
+	w.watch()
+
+	// Create a new directory
+	dir := tempDir()
+	defer os.RemoveAll(dir)
+
+	// Write config that references directory
+	cfg := fmt.Sprintf(`{"Paths": [{"Name": "%s", "Patterns": ["*"], "MaxDepth": 100}]}`, dir)
+	if err := ioutil.WriteFile(cfgFile.Name(), []byte(cfg), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	// USR2 triggers config reload
+	w.signal <- syscall.SIGUSR2
+
+	f := filepath.Join(dir, "foo")
+	if err := ioutil.WriteFile(f, []byte{0}, 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	ok, err := awaitFile(&files, f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
 		t.Errorf("want %s, got %s", f, files[0])
 	}
 }
