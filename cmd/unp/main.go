4f6cbcfe65a8c74b829e0af92372f6baae0767ee
Martin Polden <mpolden@mpolden.no>
2020-05-05 22:23:47 +0200

    Remove go-flags dependency

--- cmd/unp/main.go
+++ cmd/unp/main.go
@@ -1,33 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
-
-	flags "github.com/jessevdk/go-flags"
 
 	"github.com/mpolden/unp/rar"
 	"github.com/mpolden/unp/watcher"
 )
 
 func main() {
-	var opts struct {
-		Config string `short:"f" long:"config" description:"Config file" value-name:"FILE" default:"~/.unprc"`
-		Test   bool   `short:"t" long:"test" description:"Test and print config"`
-	}
+	configFile := flag.String("f", "~/.unprc", "Path to config file")
+	test := flag.Bool("t", false, "Test and print config")
+	flag.Parse()
 
-	_, err := flags.ParseArgs(&opts, os.Args)
-	if err != nil {
-		os.Exit(1)
-	}
-
-	cfg, err := watcher.ReadConfig(opts.Config)
+	cfg, err := watcher.ReadConfig(*configFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if opts.Test {
+	if *test {
 		json, err := cfg.JSON()
 		if err != nil {
 			log.Fatal(err)
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/mpolden/unp
 go 1.13
 
 require (
-	github.com/jessevdk/go-flags v1.4.0
 	github.com/mpolden/sfv v0.9.0
 	github.com/nwaples/rardecode v1.1.0
 	github.com/rjeczalik/notify v0.9.2
